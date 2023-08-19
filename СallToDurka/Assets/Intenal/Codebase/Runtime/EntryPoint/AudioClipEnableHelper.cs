cf5ad823bcef61b8f048d2e98b61189742634c8b
Rimuru <Rimuru.Dev@gmail.com>
2023-08-19 14:26:30 +0300

    Bug Fix

---
+++ СallToDurka/Assets/Intenal/Codebase/Runtime/EntryPoint/AudioClipEnableHelper.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using RimuruDev.Plugins.Audio.Core;
+
+namespace RimuruDev.Intenal.Codebase.Runtime.EntryPoint
+{
+    [SelectionBase]
+    [DisallowMultipleComponent]
+    public sealed class AudioClipEnableHelper : MonoBehaviour
+    {
+        [SerializeField] private List<SourceAudio> sourceAudios;
+
+        private void OnEnable()
+        {
+#if UNITY_EDITOR
+            print("AudioClipEnableHelper - OnEnable");
+#endif
+        }
+
+        private void OnDisable()
+        {
+#if UNITY_EDITOR
+            print("AudioClipEnableHelper - OnDisable");
+#endif
+            foreach (var sourceAudio in sourceAudios)
+            {
+                sourceAudio.Stop();
+                sourceAudio.Loop = false;
+            }
+        }
+
+#if UNITY_EDITOR
+        [System.Diagnostics.Conditional("DEBUG")]
+        private void OnValidate()
+        {
+            sourceAudios = new List<SourceAudio>();
+
+            foreach (var sourceAudio in FindObjectsOfType<SourceAudio>())
+                sourceAudios.Add(sourceAudio);
+        }
+#endif
+    }
+}
